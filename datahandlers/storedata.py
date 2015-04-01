174b6ecaefaf1d15b1e8ff4df4ab8616f45eb80f
liangk12 <kenneth.liang12@gmail.com>
2015-03-31 23:08:34 -0400

    placeholder script to store datasets using pandas .to_sql call. Need to refine data modeling first in datamodeler.py

---
+++ datahandlers/storedata.py
@@ -0,0 +1,11 @@
+"""
+store the data set(s)
+
+approach:
+-use sqlite db to store into local dir
+-pandas to put datasets into db
+ http://pandas.pydata.org/pandas-docs/dev/generated/pandas.DataFrame.to_sql.html
+"""
+
+import pandas as pd
+from datamodeler import * ### # IMPROVE this
