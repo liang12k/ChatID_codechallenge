d3af27405dc9b0e3a56962a958b776b4d9fcd8fa
liangk12 <kenneth.liang12@gmail.com>
2015-04-03 23:45:37 -0400

    refactored code to write mappings for 'room_id','jid' column values.

--- datahandlers/jsonparser.py
+++ datahandlers/jsonparser.py
@@ -8,10 +8,28 @@ approach:
 """
 
 import pandas as pd
+from mappings import maphandler
+import logging
 
 fulldata=pd.io.json.read_json(
     "../sampledata/sample_chat_events.json"
 ) # [340 rows x 12 columns]
+
+
+colsToMap=["room_id","jid"]
+for colname in colsToMap:
+    mappingsdict=maphandler.setmappings(
+        colname,
+        fulldata[colname].tolist()
+    )
+    fulldata[colname]=fulldata.apply(
+        lambda row: mappingsdict.get(row[colname]),
+        axis=1  
+    )
+    logging.info(
+        "%s:\nvalues:\n%s",
+        colname,str(set(fulldata[colname].values))
+    )
 
 # set 'room_id' column name as the index column
 room_idDf=fulldata.set_index(["room_id"])
--- datahandlers/mappings/maphandler.py
+++ datahandlers/mappings/maphandler.py
@@ -11,17 +11,17 @@ import ast
 import pickle
 import logging
 
-def getmapper(colName=""):
+def getmappings(colName=""):
     """
         :param str colName: column name to map to int value
         :return dict: the current mapping or blank if column name is unavailable
     """
     mappingdict={}
     try:
-        with open(colName+".txt","r") as f:
+        with open("./"+colName+".txt","r") as f:
             mappingdict=pickle.load(f)
     except IOError as excp:
-        pass # if file doesn't exist, logging.error message will notify
+        mappingdict={} # if file doesn't exist, logging.error message will notify
     if not (mappingdict and isinstance(mappingdict,dict)):
         logging.error("\nmapping for column name '%s' is unavailable\n",colName)
     return mappingdict
@@ -30,20 +30,22 @@ def setmappings(colName="",colValsAsList=[]):
     """
         :param str colName: column name to create mapping for in a .txt file
         :param list colValsAsList: the column values as a dict where key
+        :return dict: dict of column values number mappings
     """
     if not (colName and colValsAsList):
         logging.warning(
             "\nneed column name and list of its values\nentered: '%s','%s'\n",
             str(colName),str(colValsAsList)
         ); return
-    colmappings=getmapper(colName)
+    colmappings=getmappings(colName)
     # get set differences, any remaining values will need to be mapped
     colValsAsList=set(colValsAsList)-set(colmappings.keys())
     for colval in colValsAsList:
         # give column value mapping number of len(current_mappings)
         # since the first mapping number is 0
         colmappings[colval]=len(colmappings)
-    mappingfile=open(colName+".txt","ab+")
+    mappingfile=open("./"+colName+".txt","ab+")
     pickle.dump(colmappings,mappingfile)
     mappingfile.close()
     logging.info("\nsuccessfully created mappings for '%s' in mapping file: '%s.txt'", colName,colName)
+    return colmappings
